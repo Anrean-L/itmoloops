860ee9515fcef9fede1403294760275c79c8a98b
Anrean <anrean_augusta@vk.com>
2025-12-15 22:05:42 +0300

    Add notes.txt to built program

--- bin/main.cpp
+++ bin/main.cpp
@@ -5,13 +5,27 @@
 #include "song_parser/song_parser.hpp"
 #include "song_renderer.hpp"
 #include "wav_file.hpp"
+#ifndef ITMOLOOPS_NOTES_PATH
+#error "ITMOLOOPS_NOTES_PATH is not defined"
+#endif
+
+namespace {
+
+constexpr const char* kNotesPath = ITMOLOOPS_NOTES_PATH;
+
+void PrintUsage(const char* program) {
+    std::cout << "Usage: " << program << " <script.txt> <output.wav>\n";
+}
+
+}  // namespace
 
 int main(int argc, char* argv[]) {
-    if (argc < 4) {
+    if (argc < 3) {
         std::cerr << "Arguments error\n";
+        PrintUsage(argv[0]);
         return 1;
     }
-    itmoloops::FrequencyMap frequency_map(argv[2]);
+    itmoloops::FrequencyMap frequency_map(ITMOLOOPS_NOTES_PATH);
     auto composition = itmoloops::ParseComposition(argv[1], frequency_map);
     if (!composition) {
         std::cerr << "ParsingError\n";
@@ -22,7 +36,7 @@ int main(int argc, char* argv[]) {
     std::vector<int16_t> samples = composition->CreateComposition();
     std::cout << "Composition has been generated\n";
 
-    itmoloops::WavWriter(argv[3], samples);
+    itmoloops::WavWriter(argv[2], samples);
     std::cout << "Composition has been created successfully\n";
 
     return 0;
